888b413d741cccae9d4a05e54df296c3afe1d5e0
sne3ks <lenselle.nicolas@gmail.com>
2017-03-23 14:43:33 +0400

    basic function

---
+++ ACP/computervision.r
@@ -0,0 +1,12 @@
+#
+#		Analyse de donnée
+# 	Computer vision
+#
+
+image_to_matrix <- function(directory) {
+
+
+
+  A # retourne la matrice A. la derniére ligne de la fonction
+    # est la valeur retournée par la fonction.
+}
